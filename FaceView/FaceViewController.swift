24ff422f7e416d3add7b91958d4d53af745ffba3
Noah Patterson <noah@noahpatterson.me>
2016-05-25 07:15:18 -0400

    connecting faceView to model

--- FaceView/FaceViewController.swift
+++ FaceView/FaceViewController.swift
@@ -15,16 +15,27 @@ class FaceViewController: UIViewController {
         }
     }
     
-    @IBOutlet weak var faceView: FaceView!
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    private let mouthCurvatures = [FacialExpression.Mouth.Frown:-1.0,.Smirk:-0.5,.Neutral:0.0,.Grin:0.5,.Smile:1.0]
+    private let eyebrowTilts = [FacialExpression.EyeBrows.Relaxed:0.5,.Normal:0.0,.Furrowed:-0.5]
+    
     private func updateUI() {
         switch expression.eyes {
         case .Open:      faceView.eyesOpen = true
         case .Closed:    faceView.eyesOpen = false
         case .Squinting: faceView.eyesOpen = false
         }
+        // default to 0.0 if the expression is not in the lookup
+        faceView.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+        faceView.eyeBrowTilt = eyebrowTilts[expression.eyeBrows] ?? 0.0
         
-        
-        
+        faceView.lineWidth = CGFloat(1.0)
     }
+    
 }
 
